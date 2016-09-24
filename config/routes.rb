1bcc191c40b8280fc6cee382a14b9ac818a9ff8e
vuhuutuan262 <vuhuutuan262@gmail.com>
2016-09-24 21:24:26 +0700

    Layout admin page

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   get "/login", to: "session#new"
   get "/signup", to: "user#new"
   get "/home", to: "static_page#user"
+  namespace :admin do
+    root "dash_board#index"
+  end
 end
