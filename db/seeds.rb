2b3f3071b700b0e442abb60f3f229db72e9dbdd9
Chu Kim Thang <chukimthang94@gmail.com>
2016-09-29 18:09:09 +0700

    Show all + delete user

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create!(name: "Chu Kim Thang", email: "chukimthang94@gmail.com",
+  password: "123456", password_confirmation: "123456", admin: true)
+
+User.create!(name: "Vu Huu Tuan", email: "tuan@gmail.com",
+  password: "123456", password_confirmation: "123456")
+
+40.times do |n|
+  name = "user#{n}"
+  email = "email#{n}@email.com"
+  password = "123456"
+  password_confirmation = "123456"
+
+  User.create name: name, email:email, password: password,
+    password_confirmation: password_confirmation
+end
